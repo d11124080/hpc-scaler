ec3196c6ca950e65c8af3bc551fd5ff7dffbe7c1
ronan <d11124080@student.dit.ie>
2013-08-29 11:49:58 +0100

    Added preliminary IPMI driver

--- NodeController/Drivers/Ipmi/IpmiDriver.py
+++ NodeController/Drivers/Ipmi/IpmiDriver.py
@@ -1,59 +1,53 @@
 '''
-Created on 23 Aug 2013
+Created on 29 Aug 2013
 
 @author: ronan
 '''
-from importlib import import_module
-import ConfigParser
-import sys
+try:
+    from HardwareNode import HardwareNode
+    import ConfigParser
+    import sys
+except (NameError, ImportError) as e:
+    print "Component(s) not found or not readable at default location:"
+    print e
+    sys.exit(0)
 
-class IpmiDriver(object):
+
+class IpmiDriver(HardwareNode):
     '''
-    Driver for NodeController interaction with servers using the Ipmi power
-    management interface. Class acts as a wrapper for the ipmitool binary package. 
+    Ipmi Driver for the hpc-scaler NodeController. Extends the HardwareNode class to
+    provide support for the management of Ipmi interfaces using the ipmitool application,
+    which is required to be installed.
     '''
 
-    def __init__(self, configFile):
+
+    def __init__(self,cfgFile,address):
         '''
-        Cluster instance is initialised with the name of the cluster and some
-        information about the resource manager - the type, server, etc,
-        which are obtained from the configuration file we are passed on
-        instance creation
+        Constructor
         '''
-        # First, read the configuration file. We are only interested in the "ClusterInterface" section
-        # (The ClusterInterface package can be a component of an application with a shared config file)
+        self.iface = None       #
+        self.username = None    #
+        self.password = None    #
+        self.exe = None         #
+        self.cfgFile = cfgFile  #
+        self.host = address     #
+
+    def getConfig(self):
         config = ConfigParser.RawConfigParser()
-        config.read(configFile)
-        config_section = "ClusterInterface"
+        config.read(self.cfgFile)
+        config_section = "Ipmi"
         # Obtain configuration data for the cluster we will connect to.
-        self.name = config.get(config_section, "hostname")
-        self.driverPath = config.get(config_section, "driver_path")
-        self.driver = config.get(config_section, "cluster_driver")
-        # Initialise cluster components
-        self.nodelist = []  #An empty list which will hold our Node objects
-        # Attempt to load our driver, which creates a driver interface within our
-        # cluster object.
-        self.loadDriver()
-        #print "DEBUG: Created a new cluster named %s which uses the %s Driver" % (self.name, self.driver)
-        #Call buildCluster to populate the cluster node data
-        self.buildCluster()
+        self.iface = config.get(config_section, "ipmitool_interface")
+        self.exe = config.get(config_section, "ipmitool_binary")
+        self.username = config.get(config_section, "ipmi_username")
+        self.password = config.get(config_section, "ipmi_password")
 
-    def loadDriver(self):
-        '''
-        Creates an instance of the Driver interface within the cluster object
-        '''
-        try:
-            #Append the path to our driver to our python path
-            sys.path.append(self.driverPath+'/' +self.driver)
-            #dynamically load a driver we don't know the name of
-            module = import_module(self.driver +"Driver")
-            class_ = getattr(module, self.driver +"Driver")
-            #self.interface will provide an interface between the cluster instance and
-            #the driver we just loaded above
-            self.interface = class_()
-        except ImportError, e:
-            print e
-        except Exception, y:
-            print y
 
-        
+
+
+    def getStatus(self):
+        pass
+
+
+IN = IpmiDriver("../../hpc-scaler.cfg", "127.0.0.1")
+IN.getConfig()
